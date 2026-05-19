7e37c8ac22cae8ccd18b79154e1a2e402b9de709
Glueckblubbert <glueckblubbert.gamer@gmail.com>
2026-05-19 23:02:32 +0200

    Wahlen und Reden im Dialog + Zeit messen
    Fortinite y Babage

--- GameJamGame/Assets/Scripts/DialogManager.cs
+++ GameJamGame/Assets/Scripts/DialogManager.cs
@@ -18,19 +18,17 @@ public class DialogManager : MonoBehaviour
         {
             Normal, Happy, Angry,
         }
-
         private enum MCFaces
         {
             Normal, Happy, Angry, Sad, Suprised
         }
-        [SerializeField]
-        private string boxContent;
-        [SerializeField]
-        private BossFaces bossFace;
-        [SerializeField]
-        private MCFaces mCFace;
-        [SerializeField]
-        private bool isBossTalking;
+        [SerializeField] private string boxContent;
+        [SerializeField] private BossFaces bossFace;
+        [SerializeField] private MCFaces mCFace;
+        [SerializeField] private bool isBossTalking;
+        [SerializeField] private bool isChoice;
+        [SerializeField] private string[] choiceText;
+        [SerializeField] private int[] choiceBox;
 
         public string GetMessage()
         {
@@ -45,6 +43,26 @@ public class DialogManager : MonoBehaviour
         public int GetMCFace()
         {
             return (int)mCFace;
+        }
+
+        public bool HasChoice()
+        {
+            return isChoice;
+        }
+
+        public string GetChoiceText(int choice)
+        {
+            return choiceText[choice];
+        }
+
+        public int GetChoiceScreen(int choice)
+        {
+            return choiceBox[choice];
+        }
+
+        public bool IsBossTalking()
+        {
+            return isBossTalking;
         }
     }
 
@@ -78,24 +96,23 @@ public class DialogManager : MonoBehaviour
     private UnityEngine.UI.Image boss;
     [SerializeField] 
     private UnityEngine.UI.Image mC;
+    [SerializeField] private GameObject[] choiceButtons;
 
     [Space]
 
-    [SerializeField]
-    private Sprite[] bossSprites;
-    [SerializeField]
-    private Sprite[] mCSprites;
+    [SerializeField] private Sprite[] bossSprites;
+    [SerializeField] private Sprite[] bossTalkingSprites;
+    [SerializeField] private Sprite[] mCSprites;
+    [SerializeField] private Sprite[] mCTalkingSprites;
 
-    [SerializeField]
     [Range(10f, 100f)]
-    private float charsPerSecond;
-    [SerializeField]
-    private bool isTyping;
-    [SerializeField] 
-    private bool skip;
-
-    [SerializeField] 
-    private Dialogue[] dialogues;
+    [SerializeField] private float charsPerSecond;
+    [Range(1, 50)]
+    [SerializeField] private int charsPerSpriteChange;
+    [SerializeField] private bool isTyping;
+    [SerializeField] private bool skip;
+    [SerializeField] private int choice;
+    [SerializeField] private Dialogue[] dialogues;
 
     private void Awake()
     {
@@ -104,7 +121,6 @@ public class DialogManager : MonoBehaviour
 
     private void Start()
     {
-        dialogBox.SetActive(false);
         StartDialogue(0);
     }
 
@@ -114,16 +130,34 @@ public class DialogManager : MonoBehaviour
         if (dialogueNum >= dialogues.Length)
             return;
         //Spiel pausieren
-        isTyping = false;
-        skip = false;
-        print("Uiiai");
+        RefreshBox();
+        print("AHA");
         GameManager.instance.GamePauseRequest();
+        print("AHA");
         StartCoroutine(DialogRoutine(dialogueNum));
     }
 
     public void SkipText()
     {
         skip = true;
+    }
+
+    public void Choose(int option)
+    {
+        choice = option;
+    }
+
+    private void RefreshBox()
+    {
+        text.text = "";
+        isTyping = true;
+        skip = false;
+        choice = -1;
+        for (int i = 0; i < choiceButtons.Length; i++)
+        {
+            choiceButtons[i].SetActive(false);
+            choiceButtons[i].GetComponentInChildren<Text>().text = "";
+        }
     }
 
     IEnumerator DialogRoutine(int index)
@@ -133,33 +167,81 @@ public class DialogManager : MonoBehaviour
         print(boxCount);
         while (currentBox < boxCount) 
         {
-            print(currentBox);
-            text.text = "";
-            isTyping = true;
-            skip = false;
+            //besorgen der Daten der Box
             DialogueScreen currentScreen = dialogues[index].GetBox(currentBox);
             currentBox++;
-            boss.sprite = bossSprites[currentScreen.GetBossFace()];
-            mC.sprite = mCSprites[currentScreen.GetMCFace()];
+            int bossSpriteIndex = currentScreen.GetBossFace();
+            int mCSpriteIndex = currentScreen.GetMCFace();
+            mC.sprite = mCSprites[mCSpriteIndex];
+            boss.sprite = bossSprites[bossSpriteIndex];
+            //Während reden
+            int charsTalked = 0;
+            bool spriteChanged = false;
             for (int i = 0; i < currentScreen.GetMessage().Length; i++)
             {
                 if (skip)
                     break;
                 string displayedText = currentScreen.GetMessage().Insert(i, alpha);
                 text.text = displayedText;
-                print(displayedText);
+                //print(displayedText);
+                if(charsTalked % charsPerSpriteChange == 0)
+                {
+                    if (spriteChanged)
+                    {
+                        if(currentScreen.IsBossTalking())
+                        {
+                            boss.sprite = bossTalkingSprites[bossSpriteIndex];
+                        }
+                        else
+                        {
+                            mC.sprite = mCTalkingSprites[mCSpriteIndex];
+                        }
+                    }
+                    else 
+                    {
+                        if (currentScreen.IsBossTalking())
+                        {
+                            boss.sprite = bossSprites[bossSpriteIndex];
+                        }
+                        else
+                        {
+                            mC.sprite = mCSprites[mCSpriteIndex];
+                        }
+                    }
+                    spriteChanged = !spriteChanged;
+                }
                 yield return new WaitForSeconds(1f / charsPerSecond);
+                charsTalked++;
             }
-            print("writing done");
+            //Nach reden
+            mC.sprite = mCSprites[mCSpriteIndex];
+            boss.sprite = bossSprites[bossSpriteIndex];
             text.text = currentScreen.GetMessage();
             skip = false;
-            while (!skip)
+            //Warten auf Signal für nächste Box
+            if (currentScreen.HasChoice())
             {
-                yield return null;
+                for (int i = 0;i < choiceButtons.Length;i++)
+                {
+                    choiceButtons[i].SetActive(true);
+                    choiceButtons[i].GetComponentInChildren<Text>().text = currentScreen.GetChoiceText(i);
+                    yield return new WaitForSeconds(1f / charsPerSecond);
+                }
+                while (choice < 0)
+                {
+                    yield return null;
+                }
+                currentBox = currentScreen.GetChoiceScreen(choice);
             }
+            else
+            {
+                while (!skip)
+                {
+                    yield return null;
+                }
+            }
+            RefreshBox();
         }
-        text.text = "";
-        print("Dialog fertig");
         dialogBox.SetActive(false);
         GameManager.instance.GameUnpauseRequest(true);
     }
---
+++ GameJamGame/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager instance;
+    [SerializeField] private TextMeshProUGUI timeDisplay;
+    private float time;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        time = 0;
+    }
+
+    private void Update()
+    {
+        if(GameManager.paused)
+        {
+            return;
+        }
+        time += Time.deltaTime;
+        int seconds = (int)time % 60;
+        int minutes = (int)time - seconds;
+        string secString;
+        string minString;
+        if (seconds < 10)
+        {
+            secString = "0" + seconds;
+        }
+        else 
+        {
+            secString= seconds.ToString();
+        }
+        if(minutes < 10)
+        {
+            minString = "0" + minutes;
+        }
+        else
+        {
+            minString = minutes.ToString();
+        }
+        timeDisplay.text = minString + ":" + secString;
+    }
+}
